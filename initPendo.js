29273ae7b82151cf75e971a1b0e4c903de363b99
guitarkirb <guitarkirb@users.noreply.github.com>
2022-06-27 22:42:56 -0400

    Update initPendo.js

--- initPendo.js
+++ initPendo.js
@@ -5,6 +5,30 @@
             o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
         y=e.createElement(n);y.async=!0;y.src='https://pendo-dev-static.storage.googleapis.com/agent/static/'+apiKey+'/pendo.js';
         z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
+    pendo.initialize({
+    visitor: {
+        id:              user_id  // Required if user is logged in
+        // email:        // Recommended if using Pendo Feedback, or NPS Email
+        // full_name:    // Recommended if using Pendo Feedback
+        // role:         // Optional
+
+        // You can add any additional visitor level key-values here,
+        // as long as it's not one of the above reserved names.
+    },
+
+    account: {
+        id:           account_id // Required if using Pendo Feedback
+        // name:         // Optional
+        // is_paying:    // Recommended if using Pendo Feedback
+        // monthly_value:// Recommended if using Pendo Feedback
+        // planLevel:    // Optional
+        // planPrice:    // Optional
+        // creationDate: // Optional
+
+        // You can add any additional account level key-values here,
+        // as long as it's not one of the above reserved names.
+    }
+});
 })('5cef613d-7ae8-488b-5f02-7ea7944cc00d');
 //})('afcc94f9-dd69-4456-77ff-4e0082f8e702');
 
