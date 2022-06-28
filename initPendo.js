a4122a6b97233574f67e41e2c1b9d0ed5640c535
guitarkirb <guitarkirb@users.noreply.github.com>
2022-06-27 22:28:17 -0400

    Update initPendo.js

--- initPendo.js
+++ initPendo.js
@@ -5,6 +5,6 @@
             o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
         y=e.createElement(n);y.async=!0;y.src='https://pendo-dev-static.storage.googleapis.com/agent/static/'+apiKey+'/pendo.js';
         z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
-//})('5cef613d-7ae8-488b-5f02-7ea7944cc00d');
-})('afcc94f9-dd69-4456-77ff-4e0082f8e702');
+})('5cef613d-7ae8-488b-5f02-7ea7944cc00d');
+//})('afcc94f9-dd69-4456-77ff-4e0082f8e702');
 
